8c7008d6ea7f1e4fe1ef971b9a7e109d3224909d
spicytelescope <jbeziaud@gmail.com>
2020-08-06 01:15:18 +0200

    Updates

--- src/config/react-query-config.js
+++
@@ -1,8 +0,0 @@
-export const queryConfig = {
-    // in ms
-    queries: {
-        refetchOnWindowFocus: false,
-        retryDelay: 60000, // make this to false when you'll install socketIO
-        staleTime: 300 * 1000,
-    },
-}
--- src/lib/formatDuration.js
+++
@@ -1,4 +0,0 @@
-import {floor} from 'mathjs'
-const formatDuration = (duration) => `${floor(duration/60)}h${duration%60 == 0 ? "00" : duration%60}`
-
-export default formatDuration
