107a9859bd417b614d6600dcb91b050079d0b0b8
spicytelescope <jbeziaud@gmail.com>
2020-08-06 02:02:39 +0200

    Updates

---
+++ precache-manifest.73de4083c5513975f62ad14afa8a5841.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d22c76586c4589e342a96f9ce7ca97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c4855568c14151bab28",
+    "url": "/static/css/2.26b00366.chunk.css"
+  },
+  {
+    "revision": "d19a11be9d306328eeec",
+    "url": "/static/css/main.54ecd3b6.chunk.css"
+  },
+  {
+    "revision": "2c4855568c14151bab28",
+    "url": "/static/js/2.836d1e72.chunk.js"
+  },
+  {
+    "revision": "c55be5f21dc0c245eeaef6e0ca41f788",
+    "url": "/static/js/2.836d1e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19a11be9d306328eeec",
+    "url": "/static/js/main.c7826955.chunk.js"
+  },
+  {
+    "revision": "137a4073a3b9e494bea7",
+    "url": "/static/js/runtime-main.bcee10bf.js"
+  },
+  {
+    "revision": "4a876beee2e83a25f79488187ae10ad9",
+    "url": "/static/media/404_pic.4a876bee.jpg"
+  },
+  {
+    "revision": "21178e172ac4fe51e606590aa6394cd5",
+    "url": "/static/media/cyp_profil_pic.21178e17.jpg"
+  },
+  {
+    "revision": "7af0d8af522246c5740bc7c154831075",
+    "url": "/static/media/este_profil_pic.7af0d8af.jpg"
+  },
+  {
+    "revision": "963ba6fd151b18da190ddc00e19629a9",
+    "url": "/static/media/jordan_profil_pic.963ba6fd.jpg"
+  },
+  {
+    "revision": "d697ca87ea3a11a8f4759a545db9d5c1",
+    "url": "/static/media/tutorinsa_slide_1.d697ca87.jpeg"
+  },
+  {
+    "revision": "a83dd042c372916895891755cb6ffca3",
+    "url": "/static/media/tutorinsa_slide_2.a83dd042.jpeg"
+  }
+]);
