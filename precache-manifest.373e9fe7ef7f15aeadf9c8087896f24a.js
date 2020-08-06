778803f46fc531f0b24ad0f80d98f2eb29c39a8f
spicytelescope <jbeziaud@gmail.com>
2020-08-06 13:30:25 +0200

    Updates

---
+++ precache-manifest.373e9fe7ef7f15aeadf9c8087896f24a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e778358806e78fee21b6491eee10fe4e",
+    "url": "/tutorinsa-client-react/index.html"
+  },
+  {
+    "revision": "45a2ca92ac9ef9291df2",
+    "url": "/tutorinsa-client-react/static/css/2.26b00366.chunk.css"
+  },
+  {
+    "revision": "8ca64dabd884d7a0ce3d",
+    "url": "/tutorinsa-client-react/static/css/main.54ecd3b6.chunk.css"
+  },
+  {
+    "revision": "45a2ca92ac9ef9291df2",
+    "url": "/tutorinsa-client-react/static/js/2.119dd237.chunk.js"
+  },
+  {
+    "revision": "c55be5f21dc0c245eeaef6e0ca41f788",
+    "url": "/tutorinsa-client-react/static/js/2.119dd237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca64dabd884d7a0ce3d",
+    "url": "/tutorinsa-client-react/static/js/main.b01562ca.chunk.js"
+  },
+  {
+    "revision": "f5caa5d3fca6ae6b275b",
+    "url": "/tutorinsa-client-react/static/js/runtime-main.94e8bc87.js"
+  },
+  {
+    "revision": "4a876beee2e83a25f79488187ae10ad9",
+    "url": "/tutorinsa-client-react/static/media/404_pic.4a876bee.jpg"
+  },
+  {
+    "revision": "21178e172ac4fe51e606590aa6394cd5",
+    "url": "/tutorinsa-client-react/static/media/cyp_profil_pic.21178e17.jpg"
+  },
+  {
+    "revision": "7af0d8af522246c5740bc7c154831075",
+    "url": "/tutorinsa-client-react/static/media/este_profil_pic.7af0d8af.jpg"
+  },
+  {
+    "revision": "963ba6fd151b18da190ddc00e19629a9",
+    "url": "/tutorinsa-client-react/static/media/jordan_profil_pic.963ba6fd.jpg"
+  },
+  {
+    "revision": "d697ca87ea3a11a8f4759a545db9d5c1",
+    "url": "/tutorinsa-client-react/static/media/tutorinsa_slide_1.d697ca87.jpeg"
+  },
+  {
+    "revision": "a83dd042c372916895891755cb6ffca3",
+    "url": "/tutorinsa-client-react/static/media/tutorinsa_slide_2.a83dd042.jpeg"
+  }
+]);
